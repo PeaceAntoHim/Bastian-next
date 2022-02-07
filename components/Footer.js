78e0fec4f94817195addf3cbbed4600e5fe0778e
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-07 08:58:24 +0700

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-16 text-center text-white/60">
+      Copyright (c) 2022 - Design By Frans Sebastian
+    </footer>
+  );
+}
